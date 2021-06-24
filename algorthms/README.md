5fda5555954cf21194d0fd36bb646ca314ff02b9
somenzz <somenzz@163.com>
2021-06-24 16:14:44 +0800

    add file

--- algorthms/README.md
+++ algorthms/README.md
@@ -11,3 +11,41 @@
 
 代码实现 [lru_use_ordered_dict](./lru_use_ordered_dict.py)
 
+
+思考题：如果字符串是通过单链表来存储的，那该如何来判断是一个回文串呢？
+
+```java
+class Solution {
+  public boolean isPalindrome(ListNode head) {
+    if (head == null || head.next == null) {
+      return true;
+    }
+
+    ListNode prev = null;
+    ListNode slow = head;
+    ListNode fast = head;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      ListNode next = slow.next;
+      slow.next = prev;
+      prev = slow;
+      slow = next;
+    }
+
+    if (fast != null) {
+      slow = slow.next;
+    }
+
+    while (slow != null) {
+      if (slow.val != prev.val) {
+        return false;
+      }
+      slow = slow.next;
+      prev = prev.next;
+    }
+
+    return true;
+  }
+}
+```
--- algorthms/lru_use_link_table.py
+++ algorthms/lru_use_link_table.py
@@ -13,44 +13,46 @@ class LRU:
 
     def __init__(self, max_size = 10):
         self.max_size = max_size
-        self.head = Node('head')
-        self.tail = self.head
+        self.head = Node('head') #哨兵
+        self.tail = Node('tail') #尾部哨兵
+
+        self.head.next = self.tail
+        self.tail.before = self.head
+
         self.cache = {}
 
-    def __remove(self,node):
+    def __remove(self,node, delete = False):
         """
-        删除双向链表的某一个 node
+        删除双向链表的某一个 node  before - node - next
         """
-        if self.tail == node:
-            self.tail = node.before
-            self.tail.next = None
-            node.before = None
-        else:
-            before = node.before
-            next = node.next
-            before.next = next
-            next.before = before
+        before = node.before
+        next = node.next
+        before.next = next
+        next.before = before
+        if delete:
+            del node
+
 
 
     def __append_first(self,node):
-        if len(self.cache) == 0:
-            self.head.next = node
-            self.tail = node
-            node.before = self.head
-        else:
-            node.next = self.head.next
-            self.head.next.before = node
-            self.head.next = node
-            node.before = self.head
+        """
+        插入  head node ......
+        """
+
+        node.next = self.head.next
+        self.head.next.before = node
+        self.head.next = node
+        node.before = self.head
 
 
 
     def put(self,key,value):
         if key in self.cache:
-            # 缓存命中，删除当前节点，插入链表头部
-            self.__remove(self.cache[key])
+            #缓存命中，删除当前节点，插入链表头部
+            self.__remove(self.cache[key], delete = True)
         elif len(self.cache) >= self.max_size:
-            self.__remove(self.tail)
+            self.__remove(self.tail.before, delete = True)
+
         new_node = Node(value)
         self.__append_first(new_node)
         self.cache[key] = new_node
@@ -58,7 +60,7 @@ class LRU:
     def get(self,key):
         if key in self.cache:
             node = self.cache[key]
-            self.__remove(node)
+            self.__remove(node,delete=False)
             self.__append_first(node)
             return node.data
         else:
@@ -89,6 +91,8 @@ if __name__ == '__main__':
     cache.print()
     print('get 3')
     print(cache.get(3))
+    cache.print()
+    print(cache.get(5))
     cache.print()
 
 
